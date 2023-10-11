913b1043d7bf9f69d058382f754818c327368435
Franky <frank.jr.619@gmail.com>
2023-10-11 10:50:38 +0700

    custom bottom bar

--- app/src/main/java/com/fjr619/newsloc/presentation/news_navigator/BottomBar.kt
+++ app/src/main/java/com/fjr619/newsloc/presentation/news_navigator/BottomBar.kt
@@ -1,31 +1,39 @@
 package com.fjr619.newsloc.presentation.news_navigator
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavDestination
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
-import com.fjr619.newsloc.presentation.navgraph.NewsNavController
 import com.fjr619.newsloc.presentation.navgraph.Route
+import com.fjr619.newsloc.presentation.news_navigator.components.AnimatedBottomNavigationItem
 
 sealed class BottomBarScreen(
     val route: String,
@@ -58,42 +66,79 @@ fun BottomBar(
         }
 
     if (shouldShowBottomBar) {
-        NavigationBar {
-            val navBackStackEntry by navHostController.currentBackStackEntryAsState()
-            val currentDestination = navBackStackEntry?.destination
+        Box {
 
-            screens.forEach { screen ->
-                AddItem(
-                    screen = screen,
-                    currentDestination = currentDestination,
-                    onNavigateBottomBar = onNavigateBottomBar
-                )
+            var width by remember {
+                mutableFloatStateOf(0f)
             }
+
+            var currentIndex by rememberSaveable {
+                mutableIntStateOf(0)
+            }
+
+            val offsetAnim by animateFloatAsState(
+                targetValue = when (currentIndex) {
+                    1 -> width / 3
+                    2 -> (width / 3) * 2
+                    else -> 0f
+                }, label = ""
+            )
+
+            NavigationBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .onGloballyPositioned {
+                        width = it.size.width.toFloat()
+                    }
+            ) {
+                val navBackStackEntry by navHostController.currentBackStackEntryAsState()
+                val currentDestination = navBackStackEntry?.destination
+
+
+                screens.forEachIndexed { index, screen ->
+                    val selected = currentDestination?.hierarchy?.any {
+                        it.route == screen.route
+                    } == true
+
+                    if (selected) {
+                        currentIndex = index
+                    }
+
+                    AddItem(
+                        screen = screen,
+                        selected = selected,
+                        onNavigateBottomBar = {
+                            currentIndex = index
+                            onNavigateBottomBar(it)
+                        }
+                    )
+                }
+            }
+
+            Box(
+                modifier = Modifier
+                    .width(with(LocalDensity.current) { width.toDp() / 3 })
+                    .height(3.dp)
+                    .offset(with(LocalDensity.current) { offsetAnim.toDp() }, 0.dp)
+                    .clip(RoundedCornerShape(5.dp))
+                    .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.5f))
+            )
         }
+
     }
 }
 
 @Composable
 fun RowScope.AddItem(
     screen: BottomBarScreen,
-    currentDestination: NavDestination?,
+    selected: Boolean,
     onNavigateBottomBar: (BottomBarScreen) -> Unit
 ) {
-    NavigationBarItem(
-        selected = currentDestination?.hierarchy?.any {
-            it.route == screen.route
-        } == true,
-        icon = {
-            Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                Icon(
-                    modifier = Modifier.size(20.dp),
-                    imageVector = screen.icon,
-                    contentDescription = null
-                )
-                Spacer(modifier = Modifier.height(6.dp))
-                Text(text = screen.title, style = MaterialTheme.typography.labelSmall)
-            }
-        },
-        onClick = { onNavigateBottomBar(screen) }
+
+    AnimatedBottomNavigationItem(
+        selected = selected,
+        onClick = { onNavigateBottomBar(screen) },
+        icon = screen.icon,
+        label = screen.title
     )
 }

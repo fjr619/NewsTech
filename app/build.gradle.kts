db3e458a3db4124ede3796cd1035e0f4ba6f76d6
Franky Wijanarko <frank.jr.619@gmail.com>
2024-06-24 15:33:33 +0700

    update depedencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,11 +1,13 @@
+import org.jetbrains.kotlin.diagnostics.reportOnDeclaration
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-//    id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
-
     id ("kotlin-parcelize")
-    id("com.google.devtools.ksp")
+    id ("com.google.devtools.ksp")
+    id ("org.jetbrains.kotlin.plugin.compose")
 }
 
 android {
@@ -42,15 +44,17 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
     }
+
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.12"
-    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -58,28 +62,35 @@ android {
     }
 }
 
+composeCompiler {
+    enableStrongSkippingMode = true
+    includeSourceInformation = true
+    reportsDestination = layout.buildDirectory.dir("compose_compiler")
+//    stabilityConfigurationFile = rootProject.layout.projectDirectory.file("stability_config.conf")
+}
+
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.core:core-ktx:1.13.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
     implementation("androidx.activity:activity-compose:1.9.0")
-    implementation(platform("androidx.compose:compose-bom:2024.05.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.06.00"))
 
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
       implementation("androidx.compose.material:material")
     implementation("androidx.compose.material3:material3-window-size-class")
-    implementation("androidx.compose.ui:ui:1.7.0-alpha08")
-    implementation("androidx.compose.animation:animation:1.7.0-beta01")
+    implementation("androidx.compose.ui:ui:1.7.0-beta03")
+    implementation("androidx.compose.animation:animation:1.7.0-beta03")
 
 //    implementation("androidx.compose.foundation:foundation")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.05.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.06.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -111,8 +122,8 @@ dependencies {
     implementation ("androidx.datastore:datastore-preferences:1.1.1")
 
     //Paging 3
-    implementation ("androidx.paging:paging-runtime-ktx:3.2.1")
-    implementation ("androidx.paging:paging-compose:3.3.0-rc01")
+    implementation ("androidx.paging:paging-runtime-ktx:3.3.0")
+    implementation ("androidx.paging:paging-compose:3.3.0")
 
     //Room
     implementation ("androidx.room:room-runtime:2.6.1")
@@ -120,12 +131,12 @@ dependencies {
     implementation ("androidx.room:room-ktx:2.6.1")
     implementation ("androidx.room:room-paging:2.6.1")
 
-    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
+    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.8.2")
 
     implementation("androidx.biometric:biometric:1.1.0")
 
-    implementation ("androidx.compose.material3.adaptive:adaptive:1.0.0-beta01")
-    implementation ("androidx.compose.material3.adaptive:adaptive-layout:1.0.0-beta01")
-    implementation ("androidx.compose.material3.adaptive:adaptive-navigation:1.0.0-beta01")
-    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.0-beta01")
+    implementation ("androidx.compose.material3.adaptive:adaptive:1.0.0-beta03")
+    implementation ("androidx.compose.material3.adaptive:adaptive-layout:1.0.0-beta03")
+    implementation ("androidx.compose.material3.adaptive:adaptive-navigation:1.0.0-beta03")
+    implementation("androidx.compose.material3:material3-adaptive-navigation-suite:1.3.0-beta03")
 }
